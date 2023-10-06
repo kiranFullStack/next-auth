b970694c902eef802d94ddb56d4ca609e717b0e1
kiranFullStack <kiran.fullstack@outlook.com>
2023-10-06 19:14:07 +0530

    Signin working

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main>
       <h1>Next Auth Starter</h1>
+      <h2>Added Github as signin</h2>
+      <Link href='/api/auth/signin'>Signin </Link>
     </main>
   )
 }
